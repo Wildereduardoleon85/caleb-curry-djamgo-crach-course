c2b1c945da93db8e7d0adee80f7cbdf314b387bc
Wilder Leon <wleona@ripley.com>
2023-03-04 15:18:09 -0300

    Add details page

--- movies/urls.py
+++ movies/urls.py
@@ -18,7 +18,8 @@ from django.urls import path
 from movies import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home),
-    path('movies/', views.movies)
+    path("admin/", admin.site.urls),
+    path("", views.home),
+    path("movies/", views.movies),
+    path("movies/<int:id>", views.detail),
 ]
